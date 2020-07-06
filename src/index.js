1df515e8083eb64c8e403f97ee50b40396c542ff
seniorWebDev929 <fullstackdev929@hotmail.com>
2020-07-06 17:10:06 +0200

    add loading component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
+import Loading from './loading';
 import './index.css';
 
-import Artists from './Artists';
+// import Artists from './Artists';
 
+const Artists = lazy(() => import('./Artists'))
+const Performers = lazy(() => import('./performers'))
 class App extends React.Component {
     render(){
         return(
             <div className="App">
-            <Artists />
+                <Suspense fallback={<Loading />}>
+                    <Artists />
+                    <Performers />
+                </Suspense>
             </div>
         );
     }
